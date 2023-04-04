401e9080dc521fbd3e4edad6a001678483212874
Aryan Singh <98248567+3008aryansingh@users.noreply.github.com>
2023-04-04 19:50:43 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,63 +1,24 @@
-import * as api from '../api/index'
-import { AUTH, CREATE_PROFILE } from './constants'
+import { AUTH, LOGOUT, UPDATE_USER } from '../actions/constants'
 
+const authReducer =(state = {authData: null}, action)=> {
+    switch (action.type) {
+        case AUTH:
+            localStorage.setItem('profile', JSON.stringify({...action?.data}))
+            // console.log(action?.data)
+            return {...state, authData: action?.data}
 
-export const signin =(formData, openSnackbar, setLoading) => async(dispatch) => {
+            case LOGOUT:
+                localStorage.removeItem('profile')
+                return {...state, authData: null}
 
-    try {
-        //login the user
-        const { data } = await api.signIn(formData)
-
-        dispatch({ type: AUTH, data})
-        // setLoading(false)
-        openSnackbar("Signin successfull")
-        // history.push('/dashboard')
-        window.location.href="/dashboard"
-
-    } catch (error) {
-        // console.log(error?.response?.data?.message)
-        openSnackbar(error?.response?.data?.message)
-        setLoading(false)
+            case UPDATE_USER:
+                localStorage.setItem('profile', JSON.stringify({...action?.data}))
+                // console.log(action?.data)
+                return {...state, authData: action?.data}
+        
+        default:
+            return state
     }
 }
 
-export const signup =(formData, openSnackbar, setLoading) => async(dispatch) => {
-
-    try {
-        //Sign up the user
-        const { data } = await api.signUp(formData)
-        dispatch({ type: AUTH, data})
-        const { info } = await api.createProfile({name: data?.result?.name, email: data?.result?.email, userId: data?.result?._id, phoneNumber: '', businessName: '', contactAddress: '', logo: '', website: ''});
-        dispatch({ type: CREATE_PROFILE, payload: info });
-        window.location.href="/dashboard"
-        // history.push('/dashboard')
-        openSnackbar("Sign up successfull")
-
-    } catch (error) {
-        console.log(error)
-        openSnackbar(error?.response?.data?.message)
-        setLoading(false)
-    }
-}
-
-
-
-export const forgot =(formData) => async(dispatch) => {
-    try {
-        await api.forgot(formData)
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-export const reset =(formData, history) => async(dispatch) => {
-
-    try {
-        await api.reset(formData)
-        history.push('/dashboard')
-
-    } catch (error) {
-        alert(error)
-    }
-}
+export default authReducer
